61e1cd7bc18c66268ba377161c9e0683d9a197ab
mjcarnaje <michaeljamescarnaje1@gmail.com>
2021-08-07 22:55:01 +0800

    setup server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
-console.log("hello from server.ts");
+import express, { Request, Response } from "express";
+import env from "dotenv";
+
+env.config();
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Zoom clone Sever works!");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server running on port " + process.env.PORT);
+});
