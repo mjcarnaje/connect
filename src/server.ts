629eb025125bd07b9a9440cd8130f1c20fecc44c
mjcarnaje <michaeljamescarnaje1@gmail.com>
2021-08-07 23:19:34 +0800

    added pug (html framework), setup public dirname

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
-import express, { Request, Response } from "express";
+import express, { Response } from "express";
 import env from "dotenv";
 
 env.config();
 
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Zoom clone Sever works!");
-});
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (_, res: Response) => res.render("home"));
 
 app.listen(process.env.PORT, () => {
   console.log("Server running on port " + process.env.PORT);
