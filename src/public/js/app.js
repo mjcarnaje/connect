06ea48ca10dac3ac6d9e37c268bb662679dc890a
mjcarnaje <michaeljamescarnaje1@gmail.com>
2021-08-09 10:41:26 +0800

    This is so cool!!❤, web socket connection

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,13 @@
 var socket = new WebSocket("ws://" + window.location.host);
+socket.addEventListener("open", function () {
+    console.log("Connected to Server ✔!!");
+});
+socket.addEventListener("message", function (message) {
+    console.log("New message: ", message.data);
+});
+socket.addEventListener("close", function () {
+    console.log("Disconnected from Server ❌!!");
+});
+setTimeout(function () {
+    socket.send("Hello from the Browser! ❤");
+}, 5000);
--- src/server.ts
+++ src/server.ts
@@ -18,8 +18,14 @@ const server = http.createServer(app);
 // You don't have to pass the server, we just pass the server so that the can run in the same port
 const wss = new WebSocket.Server({ server });
 
-wss.on("connection", () => {
-  console.log("There is a connection");
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser ✔!!");
+
+  socket.on("close", () => console.log("Disconnected from Browswer ❌!!"));
+
+  socket.on("message", (message) => console.log("New message: " + message));
+
+  socket.send("Hello from the Server! ❤");
 });
 
 server.listen(process.env.PORT, () => {
