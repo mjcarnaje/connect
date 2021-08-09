a843ef384b62ea4b44047f03825e202daecac2bd
mjcarnaje <michaeljamescarnaje1@gmail.com>
2021-08-09 10:19:16 +0800

    setup typescript on front-end, setup websocket

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import http from "http";
-import express, { Response } from "express";
+import express from "express";
 import env from "dotenv";
 import WebSocket from "ws";
 
@@ -17,6 +17,10 @@ const server = http.createServer(app);
 
 // You don't have to pass the server, we just pass the server so that the can run in the same port
 const wss = new WebSocket.Server({ server });
+
+wss.on("connection", () => {
+  console.log("There is a connection");
+});
 
 server.listen(process.env.PORT, () => {
   console.log("Server running on port " + process.env.PORT);
