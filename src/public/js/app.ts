cf2fd57c7f8b3dd48fb2212c61c7979ff99649f0
mjcarnaje <michaeljamescarnaje1@gmail.com>
2021-08-10 11:10:51 +0800

    :package: websocket to socket.io

--- src/public/js/app.ts
+++ src/public/js/app.ts
@@ -1,45 +1,4 @@
-const messageList = document.getElementById("messageList")!;
-const messageForm = document.getElementById("messageForm")!;
-const nickNameForm = document.getElementById("nickNameForm")!;
+// @ts-ignore
+const socket = io();
 
-const socket = new WebSocket(`ws://${window.location.host}`);
-
-socket.addEventListener("open", () => {
-  console.log("Connected to Server ✔!!");
-});
-
-socket.addEventListener("message", (message) => {
-  const li = document.createElement("li")!;
-  li.innerText = message.data;
-  li.className = "items-center p-2 rounded";
-
-  messageList.append(li);
-});
-
-socket.addEventListener("close", () => {
-  console.log("Disconnected from Server ❌!!");
-});
-
-//**************************************************************** */
-
-function sendMessage(type: string, payload: string): string {
-  return JSON.stringify({ type, payload });
-}
-
-nickNameForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  const nickNameInput = nickNameForm.querySelector("input")!;
-
-  socket.send(sendMessage("nickname", nickNameInput.value));
-});
-
-messageForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  const messageInput = messageForm.querySelector("textarea")!;
-
-  socket.send(sendMessage("newMessage", messageInput.value));
-
-  messageInput.value = "";
-});
+console.log(socket);
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import http from "http";
 import express from "express";
 import env from "dotenv";
-import WebSocket from "ws";
+import { Server } from "socket.io";
 
 env.config();
 
@@ -14,35 +14,40 @@ app.use(express.static(__dirname + "/public"));
 // });
 
 const server = http.createServer(app);
+const io = new Server(server);
 
-// You don't have to pass the server, we just pass the server so that the can run in the same port
-const wss = new WebSocket.Server({ server });
-
-type ISocket = WebSocket & { nickname: string };
-
-const sockets: WebSocket[] = [];
-
-wss.on("connection", (socket: ISocket) => {
-  sockets.push(socket);
-  socket["nickname"] = "Anonymous";
-  console.log("Connected to Browser ✔!!");
-  socket.on("close", () => console.log("Disconnected from Browswer ❌!!"));
-  socket.on("message", (msg) => {
-    const message = JSON.parse(msg.toString());
-    switch (message.type) {
-      case "newMessage":
-        sockets.forEach((_socket) => {
-          _socket.send(`${socket.nickname}: ${message.payload}`);
-        });
-        break;
-      case "nickname":
-        socket["nickname"] = message.payload;
-        break;
-      default:
-        break;
-    }
-  });
+io.on("connection", (socket) => {
+  console.log(socket);
 });
+
+// // You don't have to pass the server, we just pass the server so that the can run in the same port
+// const wss = new WebSocket.Server({ server });
+
+// type ISocket = WebSocket & { nickname: string };
+
+// const sockets: WebSocket[] = [];
+
+// wss.on("connection", (socket: ISocket) => {
+//   sockets.push(socket);
+//   socket["nickname"] = "Anonymous";
+//   console.log("Connected to Browser ✔!!");
+//   socket.on("close", () => console.log("Disconnected from Browswer ❌!!"));
+//   socket.on("message", (msg) => {
+//     const message = JSON.parse(msg.toString());
+//     switch (message.type) {
+//       case "newMessage":
+//         sockets.forEach((_socket) => {
+//           _socket.send(`${socket.nickname}: ${message.payload}`);
+//         });
+//         break;
+//       case "nickname":
+//         socket["nickname"] = message.payload;
+//         break;
+//       default:
+//         break;
+//     }
+//   });
+// });
 
 server.listen(process.env.PORT, () => {
   console.log("Server running on port " + process.env.PORT);
