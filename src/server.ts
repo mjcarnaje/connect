ddf6160e8ba9f94c5a506a959b36819d4f2df491
mjcarnaje <michaeljamescarnaje1@gmail.com>
2021-11-17 11:43:29 +0800

    revert socket.io commits

--- src/server.ts
+++ src/server.ts
@@ -1,58 +1,49 @@
-import "dotenv/config";
 import http from "http";
 import express from "express";
-import { Server } from "socket.io";
+import env from "dotenv";
+import WebSocket from "ws";
+
+env.config();
 
 const app = express();
 
-app.use(express.static(__dirname + "/../client/public"));
+app.use(express.static(__dirname + "/public"));
+
+// app.listen(process.env.PORT, () => {
+//   console.log("Server running on port " + process.env.PORT);
+// });
 
 const server = http.createServer(app);
-const io = new Server(server);
 
-io.on("connection", (socket) => {
-  socket.on("enterRoom", (roomName, done) => {
-    console.log(roomName);
+// You don't have to pass the server, we just pass the server so that the can run in the same port
+const wss = new WebSocket.Server({ server });
 
-    setTimeout(() => {
-      done();
-    }, 5000);
+type ISocket = WebSocket & { nickname: string };
+
+const sockets: WebSocket[] = [];
+
+wss.on("connection", (socket: ISocket) => {
+  sockets.push(socket);
+  socket["nickname"] = "Anonymous";
+  console.log("Connected to Browser ✔!!");
+  socket.on("close", () => console.log("Disconnected from Browswer ❌!!"));
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg.toString());
+    switch (message.type) {
+      case "newMessage":
+        sockets.forEach((_socket) => {
+          _socket.send(`${socket.nickname}: ${message.payload}`);
+        });
+        break;
+      case "nickname":
+        socket["nickname"] = message.payload;
+        break;
+      default:
+        break;
+    }
   });
 });
 
 server.listen(process.env.PORT, () => {
   console.log("Server running on port " + process.env.PORT);
 });
-
-// app.listen(process.env.PORT, () => {
-//   console.log("Server running on port " + process.env.PORT);
-// });
-
-// // You don't have to pass the server, we just pass the server so that the can run in the same port
-// const wss = new WebSocket.Server({ server });
-
-// type ISocket = WebSocket & { nickname: string };
-
-// const sockets: WebSocket[] = [];
-
-// wss.on("connection", (socket: ISocket) => {
-//   sockets.push(socket);
-//   socket["nickname"] = "Anonymous";
-//   console.log("Connected to Browser ✔!!");
-//   socket.on("close", () => console.log("Disconnected from Browswer ❌!!"));
-//   socket.on("message", (msg) => {
-//     const message = JSON.parse(msg.toString());
-//     switch (message.type) {
-//       case "newMessage":
-//         sockets.forEach((_socket) => {
-//           _socket.send(`${socket.nickname}: ${message.payload}`);
-//         });
-//         break;
-//       case "nickname":
-//         socket["nickname"] = message.payload;
-//         break;
-//       default:
-//         break;
-//     }
-//   });
-// });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mode: "jit",
-  purge: ["./client/public/index.html"],
+  purge: ["./src/**/*.html"],
   darkMode: false,
   theme: {
     fontFamily: {
@@ -10,5 +10,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [],
 };
