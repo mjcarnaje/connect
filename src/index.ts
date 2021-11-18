c5e949ecfbbce2f210e7048c11e1a68662ab8fad
mjcarnaje <michaeljamescarnaje1@gmail.com>
2021-11-18 10:39:56 +0800

    refactor

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ const port = 3000;
 
 app.set("views", path.join(__dirname + "/views"));
 app.set("view engine", "pug");
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   res.render("index", { title: "Zoom Clone!" });
